d9f1f1d544d1a84eb588946a20a2269a9b6a1220
SirNeon <sirneon618@gmail.com>
2016-03-25 11:52:07 -0400

    script client

--- lib/NeonRAW/clients/script.rb
+++ lib/NeonRAW/clients/script.rb
@@ -1,14 +1,32 @@
-require 'faraday'
-require 'multi_json'
+require 'securerandom'
+require_relative 'base'
 
 module NeonRAW
   # le script
-  class Script
-    headers = { 'User-Agent' => "Powered by NeonRAW v#{NeonRAW::VERSION}",
-                'Authorization' => Faraday.basic_auth(@client_id, @secret) }
-    conn = Faraday.new(url: 'https://www.reddit.com',
-                       headers: headers) do |faraday|
-      faraday.adapter :typhoeus
+  class Script < Base
+    def initialize(username, password, client_id, secret, opts = {})
+      @username = username
+      @password = password
+      @client_id = client_id
+      @secret = secret
+      @redirect_uri = opts[:redirect_uri] || 'http://127.0.0.1:'
+      @user_agent = opts[:user_agent] || "NeonRAW v#{NeonRAW::VERSION}"
+    end
+
+    # rubocop:disable Metrics/MethodLength
+    def authorize!
+      connection.get do |req|
+        req.url = '/api/v1/authorize'
+        req.params['client_id'] = @client_id
+        req.params['secret'] = @secret
+        req.params['redirect_uri'] = @redirect_uri
+        req.params['response_type'] = 'code'
+        req.params['state'] = SecureRandom.hex(12)
+        req.params['duration'] = 'permanent'
+        req.params['scope'] = 'identity edit flair history modconfig\
+        modflair modlog modposts modwiki mysubreddits privatemessages\
+        read report save submit subscribe vote wikiedit wikiread'
+      end
     end
   end
 end
