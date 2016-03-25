397d57efcc25c0acd8f39598fe8649cd72537d1e
SirNeon <sirneon618@gmail.com>
2016-03-25 11:50:30 -0400

    creates client

--- lib/NeonRAW.rb
+++ lib/NeonRAW.rb
@@ -1,8 +1,10 @@
 # rubocop:disable Style/FileName
 require 'NeonRAW/version'
+require 'NeonRAW/clients/script'
 
 # le module
 module NeonRAW
-  def self.new
+  def self.new(username, password, client_id, secret, opts = {})
+    Client.new(username, password, client_id, secret, opts)
   end
 end
