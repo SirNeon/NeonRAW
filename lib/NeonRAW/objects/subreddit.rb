0472d712b0788299537050ac9cd8e07c473e1a29
SirNeon <sirneon618@gmail.com>
2016-03-26 15:02:08 -0400

    added subreddit object

--- lib/NeonRAW/objects/subreddit.rb
+++ lib/NeonRAW/objects/subreddit.rb
@@ -1,8 +1,28 @@
 module NeonRAW
-  class Subreddit
-    attr_reader :data
-    def initialize(name)
-      @data = request_data("/r/#{name}/about.json")[:data]
+  module Objects
+    # le subreddit object
+    class Subreddit
+      # rubocop:disable Metrics/MethodLength
+      def initialize(data)
+        data.each do |key, value|
+          self.class.send(:define_method, key) do
+            instance_variable_set(:"@#{key}", value)
+          end
+        end
+        class << self
+          alias_method :subscribed?, :user_is_subscriber
+          alias_method :approved_submitter?, :user_is_contributor
+          alias_method :moderator?, :user_is_moderator
+          alias_method :im_banned?, :user_is_banned
+          alias_method :collapse_deleted_comments?, :collapse_deleted_comments
+          alias_method :nsfw?, :over18
+          alias_method :muted?, :user_is_muted
+          alias_method :quarantined?, :quarantine
+          alias_method :public_traffic?, :public_traffic
+          alias_method :theme_enabled?, :user_sr_theme_enabled
+          alias_method :wiki_enabled?, :wiki_enabled
+        end
+      end
     end
   end
 end
