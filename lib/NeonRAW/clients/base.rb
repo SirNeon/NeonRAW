53c617353ca1fb4a721ce07e5f9ecbf309e5b7b5
SirNeon <sirneon618@gmail.com>
2016-03-27 05:55:17 -0400

    added me method

--- lib/NeonRAW/clients/base.rb
+++ lib/NeonRAW/clients/base.rb
@@ -7,17 +7,17 @@ require_relative '../objects/access'
 module NeonRAW
   # The underlying base for the client
   class Base
-    def headers
+    def api_headers
       {
         'User-Agent' => @user_agent,
         'Authorization' => "bearer #{@access.access_token}"
       }
     end
 
-    def connection
-      @connection ||= Faraday.new(
+    def api_connection
+      @api_connection = Faraday.new(
         'https://oauth.reddit.com',
-        headers: headers
+        headers: api_headers
       )
     end
 
@@ -29,14 +29,14 @@ module NeonRAW
     end
 
     def auth_connection
-      @auth_connection ||= Faraday.new(
+      @auth_connection = Faraday.new(
         'https://www.reddit.com',
         headers: auth_headers
       )
     end
 
-    def request_data(path, params = {})
-      data = connection.get do |req|
+    def request_data(path, meth, params = {})
+      data = api_connection.send :"#{meth}" do |req|
         req.url(path)
         req.params = params
       end
@@ -44,13 +44,18 @@ module NeonRAW
     end
 
     def get_subreddit(name)
-      data = request_data("/r/#{name}/about.json")[:data]
+      data = request_data("/r/#{name}/about.json", 'get')[:data]
       Objects::Subreddit.new(data)
     end
 
     def get_user(name)
-      data = request_data("/user/#{name}/about.json")[:data]
+      data = request_data("/user/#{name}/about.json", 'get')[:data]
       Objects::User.new(data)
+    end
+
+    def me
+      data = request_data('/api/v1/me', 'get')
+      Objects::Me.new(data)
     end
   end
 end
