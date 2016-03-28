4992afe66e14d159cc1cd9041a391dfb924f6ae2
SirNeon <sirneon618@gmail.com>
2016-03-28 04:07:34 -0400

    removed redundant documentation

--- lib/NeonRAW/objects/me.rb
+++ lib/NeonRAW/objects/me.rb
@@ -19,33 +19,6 @@ module NeonRAW
     # @!attribute [r] over_18?
     #   @return [Boolean] Returns whether or not you can view adult
     #     content.
-    # @!attribute [r] friend?
-    #   @return [Boolean] Returns whether or not the user is a friend.
-    # @!attribute [r] gold?
-    #   @return [Boolean] Returns whether or not the user has gold.
-    # @!attribute [r] moderator?
-    #   @return [Boolean] Returns whether or not the user is a
-    #     moderator.
-    # @!attribute [r] verified_email?
-    #   @return [Boolean] Returns whether or not the user has a
-    #     verified email.
-    # @!attribute [r] hide_from_robots?
-    #   @return [Boolean] Returns whether or not the user doesn't
-    #     want web crawlers indexing their profile page.
-    # @!attribute [r] name
-    #   @return [String] Returns the user's name.
-    # @!attribute [r] created
-    #   @return [Float] Returns when the user's account was created
-    #     (UNIX timestamp)
-    # @!attribute [r] created_utc
-    #   @return [Float] Returns when the user's account was created
-    #     in UTC (UNIX timestamp).
-    # @!attribute [r] link_karma
-    #   @return [Integer] Returns the link karma of the user.
-    # @!attribute [r] comment_karma
-    #   @return [Integer] Returns the comment karma of the user.
-    # @!attribute [r] id
-    #   @return [String] Returns the id of the user.
     class Me < User
       # rubocop:disable Metrics/MethodLength
       def initialize(data)
