7390586b32ccc201a111bea78db619d1916edac6
SirNeon <sirneon618@gmail.com>
2016-03-28 23:11:37 -0400

    doc updates and added listing methods

--- lib/NeonRAW/objects/subreddit.rb
+++ lib/NeonRAW/objects/subreddit.rb
@@ -1,3 +1,7 @@
+require_relative '../objects/submission'
+require_relative '../objects/listing'
+require_relative '../objects/comment'
+
 module NeonRAW
   module Objects
     # le subreddit object
@@ -38,43 +42,53 @@ module NeonRAW
     #   @return [Boolean] Returns whether or not the subreddit hides
     #     ads.
     # @!attribute [r] banner_img
-    #   @return [String] Returns a string container a link to the
-    #     banner image.
+    #   @return [String, nil] Returns a string container a link to the
+    #     banner image or nil if there is none.
     # @!attribute [r] submit_text_html
-    #   @return [String] Returns the text with HTML included in the
-    #     thread submission page.
+    #   @return [String, nil] Returns the text with HTML included in the
+    #     thread submission page or nil if there is none..
     # @!attribute [r] id
     #   @return [String] Returns the id of the subreddit.
     # @!attribute [r] submit_text
-    #   @return [String] Returns the text included in the thread
-    #     submission page.
+    #   @return [String, nil] Returns the text included in the thread
+    #     submission page or nil if there is none.
     # @!attribute [r] display_name
     #   @return [String] Returns the display name of the subreddit.
     # @!attribute [r] header_img
-    #   @return [String] Returns the link to the header image.
-    # @!attribute [r] description_html
-    #   @return [String] Returns the subreddit description with HTML.
+    #   @return [String, nil] Returns the link to the header image
+    #     or nil if there is none.
+    # @!attribute [r] sidebar_html
+    #   @return [String, nil] Returns the subreddit's sidebar text
+    #     with HTML or nil if there is none..
     # @!attribute [r] title
     #   @return [String] Returns the subreddit's title.
     # @!attribute [r] public_description
-    #   @return [String] Returns the subreddit's public description.
+    #   @return [String, nil] Returns the subreddit's public description or nil
+    #     if there is none..
     # @!attribute [r] public_description_html
-    #   @return [String] Returns the subreddit's public description
-    #     with HTML included.
+    #   @return [String, nil] Returns the subreddit's public description
+    #     with HTML included or nil if there is none.
     # @!attribute [r] suggested_comment_sort
     #   @return [String, nil] Returns the subreddit's suggested
-    #     comment sort or nil if there isn't one.
+    #     comment sort or nil if there isn't one [hot, top, new,
+    #     old, controversial, random]
+    # @!attribute [r] submit_link_label
+    #   @return [String, nil] Returns the subreddit's custom label
+    #     for the submit link button or nil if there is none.
+    # @!attribute [r] submit_text_label
+    #   @return [String, nil] Returns the subreddit's custom label
+    #     for the submit text button or nil if there is none.
     # @!attribute [r] header_title
-    #   @return [String] Returns a string containing the header
-    #     title.
-    # @!attribute [r] description
-    #   @return [String] Returns the subreddit's description.
+    #   @return [String, nil] Returns the header title or nil if
+    #     there is none.
+    # @!attribute [r] sidebar
+    #   @return [String] Returns the subreddit's sidebar text.
     # @!attribute [r] accounts_active
     #   @return [Integer] Returns the number of users online browsing
     #     the subreddit.
     # @!attribute [r] header_size
-    #   @return [Array] Returns an array containing the header's
-    #     dimensions.
+    #   @return [Array, nil] Returns an array containing the
+    #     header's dimensions or nil if there isn't one.
     # @!attribute [r] subscribers
     #   @return [Integer] Returns the number of subscribers the
     #     subreddit has.
@@ -95,24 +109,26 @@ module NeonRAW
     #     scores are hidden.
     # @!attribute [r] subreddit_type
     #   @return [String] Returns the type of subreddit [public,
-    #     restricted, private].
+    #     restricted, private, gold_restricted, archived].
     # @!attribute [r] submission_type
     #   @return [String] Returns the type of submissions allowed
     #     to be posted [any, link, self].
     class Subreddit
       # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
-      def initialize(data)
+
+      def initialize(client, data)
+        @client = client
         data.each do |key, value|
-          self.class.send(:define_method, key) do
-            instance_variable_set(:"@#{key}", value)
-          end
+          value = nil if ['', [], {}].include?(value)
+          instance_variable_set(:"@#{key}", value)
+          self.class.send(:attr_reader, key)
         end
         class << self
           alias_method :subscribed?, :user_is_subscriber
           alias_method :approved_submitter?, :user_is_contributor
           alias_method :moderator?, :user_is_moderator
           alias_method :im_banned?, :user_is_banned
-          alias_method :collapse_deleted_comments?, :collapse_deleted_comments
+          alias_method :collapses_deleted_comments?, :collapse_deleted_comments
           alias_method :nsfw?, :over18
           alias_method :muted?, :user_is_muted
           alias_method :quarantined?, :quarantine
@@ -120,6 +136,31 @@ module NeonRAW
           alias_method :theme_enabled?, :user_sr_theme_enabled
           alias_method :wiki_enabled?, :wiki_enabled
           alias_method :hide_ads?, :hide_ads
+          alias_method :sidebar, :description
+          alias_method :sidebar_html, :description_html
+        end
+      end
+
+      # Fetches a listing from the subreddit.
+      # @!method get_hot(params = { limit: 25 })
+      # @!method get_top(params = { limit: 25 })
+      # @!method get_old(params = { limit: 25 })
+      # @!method get_new(params = { limit: 25 })
+      # @!method get_controversial(params = { limit: 25 })
+      # @!method get_comments(param = { limit: 25 })
+      # @param params [Hash] The parameters for the request.
+      # @option params :t [String] Time for relevant sorting [hour, day, week,
+      #   month, year, all]
+      # @option params :after [String] The name of the next data block.
+      # @option params :before [String] The name of the previous data block.
+      # @option params :count [Integer] The number of items already in the
+      #   listing.
+      # @option params :show [String] Literally the string 'all'.
+      # @return [NeonRAW::Objects::Listing] Returns the listing object.
+      %w(hot top old new controversial comments).each do |type|
+        define_method :"get_#{type}" do |params = { limit: 25 }|
+          path = "/r/#{display_name}/#{type}/.json"
+          @client.send(:build_listing, path, params)
         end
       end
     end
