0f106bac4da3c30f86d8b2d619b53b941b4df0c3
SirNeon <sirneon618@gmail.com>
2016-03-28 23:07:55 -0400

    added comment object

--- lib/NeonRAW/objects/comment.rb
+++ lib/NeonRAW/objects/comment.rb
@@ -2,8 +2,10 @@ module NeonRAW
   module Objects
     # le comment object
     class Comment
-      def initialize(data)
+      def initialize(client, data)
+        @client = client
         data.each do |key, value|
+          value = nil if ['', [], {}].include?(value)
           instance_variable_set(:"@#{key}", value)
           self.class.send(:attr_reader, key)
         end
