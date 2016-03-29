ba3c1fbce503e75397c3a1552410fa6e79ce69b8
SirNeon <sirneon618@gmail.com>
2016-03-29 18:08:59 -0400

    publicized define_method

--- lib/NeonRAW/objects/thing.rb
+++ lib/NeonRAW/objects/thing.rb
@@ -4,6 +4,9 @@ module NeonRAW
   module Objects
     # Exists to hold methods that work in all objects.
     class Thing
+      class << self
+        public :define_method
+      end
       include Votable
     end
   end
