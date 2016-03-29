b9da76ec3063bff5cc23b68488210f02b037e30b
SirNeon <sirneon618@gmail.com>
2016-03-28 23:07:40 -0400

    added listing builder method

--- lib/NeonRAW/clients/base.rb
+++ lib/NeonRAW/clients/base.rb
@@ -66,19 +66,49 @@ module NeonRAW
       JSON.parse(data.body, symbolize_names: true)
     end
 
+    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+
+    # Creates the listing object.
+    # @!method build_listing(path, params)
+    # @param path [String] The API path for the listing.
+    # @param params [Hash] The parameters for the request.
+    # @return [NeonRAW::Objects::Listing] Returns the Listing object.
+    def build_listing(path, params)
+      data_arr = []
+      until data_arr.length == params[:limit]
+        data = request_data(path, 'get', params)
+        params[:after] = data[:data][:after]
+        params[:before] = data[:data][:before]
+        data[:data][:children].each do |item|
+          if item[:kind] == 't3'
+            data_arr << Objects::Submission.new(self, item[:data])
+          end
+          if item[:kind] == 't1'
+            data_arr << Objects::Comment.new(self, item[:data])
+          end
+          break if data_arr.length == params[:limit]
+        end
+        break if params[:after].nil?
+      end
+      listing = Objects::Listing.new(params[:after], params[:before])
+      data_arr.each { |submission| listing << submission }
+      listing
+    end
+
     def get_subreddit(name)
       data = request_data("/r/#{name}/about.json", 'get')[:data]
-      Objects::Subreddit.new(data)
+      Objects::Subreddit.new(self, data)
     end
 
     def get_user(name)
       data = request_data("/user/#{name}/about.json", 'get')[:data]
-      Objects::User.new(data)
+      Objects::User.new(self, data)
     end
 
     def me
       data = request_data('/api/v1/me', 'get')
-      Objects::Me.new(data)
+      Objects::Me.new(self, data)
     end
+    private :build_listing
   end
 end
