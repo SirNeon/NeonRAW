9e1db80a5c5d184bbc8b181dec67974b9498e6c3
SirNeon <sirneon618@gmail.com>
2016-03-29 20:15:58 -0400

    made helper method private

--- lib/NeonRAW/objects/thing/gildable.rb
+++ lib/NeonRAW/objects/thing/gildable.rb
@@ -11,5 +11,23 @@ module NeonRAW
         false
       end
     end
+
+    # Give a thing gold.
+    # @!method gild
+    # @param quantity [Integer] The amount of gold to give.
+    def gild(quantity)
+      quantity.times do
+        @client.request_data("/api/v1/gold/gild/#{name}", :post)
+        update_gild_count(1)
+      end
+    end
+
+    # Updates the gold count of a thing.
+    # @!method update_gild_count(count)
+    # @param count [1..36]
+    def update_gild_count(count)
+      @gilded += count
+    end
+    private :update_gild_count
   end
 end
