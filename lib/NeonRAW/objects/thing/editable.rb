768c98528df884c8688b80acf6c7234ea8146e39
SirNeon <sirneon618@gmail.com>
2016-03-29 20:15:34 -0400

    added method to edit things

--- lib/NeonRAW/objects/thing/editable.rb
+++ lib/NeonRAW/objects/thing/editable.rb
@@ -21,5 +21,33 @@ module NeonRAW
     def last_edit
       nil || @edited if @edited != false
     end
+
+    # Edit a thing.
+    # @!method edit(text)
+    # @param text [String] The text to replace the current text with.
+    def edit(text)
+      params = {}
+      params[:api_type] = 'json'
+      params[:text] = text
+      params[:thing_id] = name
+      @client.request_data('/api/editusertext', :post, params)
+    end
+
+    # Updates the text body of the thing.
+    # @!method update_text_body(kind, body, body_html)
+    # @param kind [String] The type of object [t1, t3]
+    # @param body [String] The new text to replace the old text.
+    # @param body_html [String] The new text with HTML to replace the old text
+    #   with HTML.
+    def update_text_body(kind, body, body_html)
+      if kind == 't1'
+        @body = body
+        @body_html = body_html
+      elsif kind == 't3'
+        @selftext = body
+        @selftext_html = body_html
+      end
+    end
+    private :update_text_body
   end
 end
