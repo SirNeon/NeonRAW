67d148cff260a987124df2fcc70b880eaab0246d
SirNeon <sirneon618@gmail.com>
2016-03-31 15:23:15 -0400

    added methods for things that show up in your inbox

---
+++ lib/NeonRAW/objects/thing/inboxable.rb
@@ -0,0 +1,30 @@
+module NeonRAW
+  module Objects
+    class Thing
+      # Methods for things that appear in your inbox.
+      module Inboxable
+        # Changes the read status of a PM.
+        # @!method mark_as_read
+        # @!method mark_as_unread
+        %w(read unread).each do |type|
+          define_method :"mark_as_#{type}" do
+            params = {}
+            params[:id] = name
+            @client.request_data("/api/#{type}_message", :post, params)
+          end
+        end
+
+        # Add a text reply to a comment/private message.
+        # @!method reply(text)
+        # @param text [String] The text you want to reply with.
+        def reply(text)
+          params = {}
+          params[:api_type] = 'json'
+          params[:text] = text
+          params[:thing_id] = name
+          @client.request_data('/api/comment', :post, params)
+        end
+      end
+    end
+  end
+end
