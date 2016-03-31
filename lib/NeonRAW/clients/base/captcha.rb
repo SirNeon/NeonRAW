b6bb672da619c9e729b5140f3c60cbe097c1caf4
SirNeon <sirneon618@gmail.com>
2016-03-31 19:35:07 -0400

    added docs

--- lib/NeonRAW/clients/base/captcha.rb
+++ lib/NeonRAW/clients/base/captcha.rb
@@ -2,18 +2,23 @@ module NeonRAW
   class Base
     # Methods for captchas.
     module Captchas
+      # Checks whether captchas are needed for API methods that define the
+      # captcha and iden parameters.
+      # @!method needs_captcha?
+      # @return [String] Returns "true" or "false".
       def needs_captcha?
-        request_data('/api/needs_captcha', :get)
+        request_nonjson('/api/needs_captcha', :get)
       end
 
+      # Fetches a captcha link for you.
+      # @!method new_captcha
+      # @return [String] Returns a link to the captcha image.
       def new_captcha
         params = {}
         params[:api_type] = 'json'
-        request_data('/api/new_captcha', :post, params)
-      end
-
-      def captcha_iden
-        request_data("/captcha/#{iden}", :get)
+        data = request_data('/api/new_captcha', :post, params)
+        iden = data[:json][:data][:iden]
+        "https://www.reddit.com/captcha/#{iden}.png"
       end
     end
   end
