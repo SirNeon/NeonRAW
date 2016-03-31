f40b4a43f10f79bcdcb54988d43bcfe255c60705
SirNeon <sirneon618@gmail.com>
2016-03-31 17:51:13 -0400

    trophy object

--- lib/NeonRAW/objects/trophy.rb
+++ lib/NeonRAW/objects/trophy.rb
@@ -1,7 +1,19 @@
+require_relative 'thing'
+
 module NeonRAW
   module Objects
     # le Trophy object
-    class Trophy
+    # @!attribute [r] icon_70
+    #   @return [String] Returns a link to the icon file.
+    # @!attribute [r] description
+    #   @return [String, nil] Returns the description or nil if there is none.
+    # @!attribute [r] url
+    #   @return [String, nil] Returns the URL or nil if there is none.
+    # @!attribute [r] icon_40
+    #   @return [String] Returns a link to the icon file.
+    # @!attribute [r] award_id
+    #   @return [String] Returns the award ID.
+    class Trophy < Thing
       def initialize(data)
         data.each do |key, value|
           instance_variable_set(:"@#{key}", value)
