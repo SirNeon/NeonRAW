7798a8542094b50fad7371e50facf3a834b63224
SirNeon <sirneon618@gmail.com>
2016-04-03 16:19:02 -0400

    added method to reply to things

---
+++ lib/NeonRAW/objects/thing/repliable.rb
@@ -0,0 +1,28 @@
+# rubocop:disable Metrics/MethodLength
+
+module NeonRAW
+  module Objects
+    class Thing
+      # Methods for things that can be replied to.
+      module Repliable
+        # Leaves a comment/PM reply to the thing.
+        # @!method reply(text)
+        # @param text [String] The text body of the comment.
+        # @return [NeonRAW::Objects::Comment/PrivateMessage] Returns the object.
+        def reply(text)
+          params = {}
+          params[:api_type] = 'json'
+          params[:text] = text
+          params[:thing_id] = name
+          data = @client.request_data('/api/comment', :post, params)
+          object_data = data[:json][:data][:things][0][:data]
+          if data[:kind] == 't1'
+            Comment.new(@client, object_data)
+          elsif data[:kind] == 't4'
+            PrivateMessage.new(@client, object_data)
+          end
+        end
+      end
+    end
+  end
+end
