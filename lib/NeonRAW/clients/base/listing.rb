7890e4cb0de3ec4671442a5d68fce3315df561e1
SirNeon <sirneon618@gmail.com>
2016-04-05 22:52:22 -0400

    added docs and multireddit method

--- lib/NeonRAW/clients/base/listing.rb
+++ lib/NeonRAW/clients/base/listing.rb
@@ -4,6 +4,7 @@ require_relative '../../objects/me'
 require_relative '../../objects/access'
 require_relative '../../objects/privatemessage'
 require_relative '../../objects/modlogaction'
+require_relative '../../objects/multireddit'
 require_relative '../../errors'
 # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
 
@@ -42,19 +43,44 @@ module NeonRAW
           listing
         end
 
+        # Creates a subreddit object.
+        # @!method get_subreddit(name)
+        # @param name [String] The name of the subreddit.
+        # @return [NeonRAW::Objects::Subreddit] Returns the subreddit object.
         def get_subreddit(name)
           data = request_data("/r/#{name}/about.json", :get)[:data]
           Objects::Subreddit.new(self, data)
         end
 
+        # Creates a user object.
+        # @!method get_user(name)
+        # @param name [String] The name of the user.
+        # @return [NeonRAW::Objects::User] Returns the user object.
         def get_user(name)
           data = request_data("/user/#{name}/about.json", :get)[:data]
           Objects::User.new(self, data)
         end
 
+        # Creates a me object.
+        # @!method me
+        # @return [NeonRAW::Objects::Me] Returns the me object.
         def me
           data = request_data('/api/v1/me', :get)
           Objects::Me.new(self, data)
+        end
+
+        # Creates a multireddit object.
+        # @!method get_multireddit(multireddit_path)
+        # @param multireddit_path [String] The path to the multireddit (e.g.
+        #   /user/username/m/multireddit_name).
+        # @return [NeonRAW::Objects::MultiReddit] Returns the multireddit
+        #   object.
+        def get_multireddit(multireddit_path)
+          params = {}
+          params[:multipath] = multireddit_path
+          params[:expand_srs] = false
+          data = request_data("/api/multi/#{multireddit_path}", :get, params)
+          Objects::MultiReddit.new(self, data[:data])
         end
         private :build_listing
       end
