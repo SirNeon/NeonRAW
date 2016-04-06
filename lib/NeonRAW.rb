dee92f64186aa63d8cc7c32b6ef19aab27e71359
SirNeon <sirneon618@gmail.com>
2016-04-06 01:52:13 -0400

    changed syntax for the umpteenth time

--- lib/NeonRAW.rb
+++ lib/NeonRAW.rb
@@ -1,24 +1,10 @@
-# rubocop:disable Style/FileName, Metrics/ParameterLists
+# rubocop:disable Style/FileName
 require 'NeonRAW/version'
 require 'NeonRAW/clients/script'
 
 # le module
 module NeonRAW
-  TYPES = {
-    script: Clients::Script
-  }.freeze
-
-  # Create a new client instance.
-  # @!method self.new(type, username, password, client_id, secret, opts = {})
-  # @param type [Symbol] The type of client to make [script, web, installed].
-  # @param username [String] Your account's username.
-  # @param password [String] Your account's password.
-  # @param client_id [String] Your account's client_id.
-  # @param secret [String] Your account's secret.
-  # @param opts [Hash] Optional parameters.
-  # @option opts :user_agent [String] The useragent for the client.
-  # @option opts :redirect_uri [String] The redirect URI for the client.
-  def self.new(type, username, password, client_id, secret, opts = {})
-    TYPES[type].new(username, password, client_id, secret, opts)
+  def self.script(username, password, client_id, secret, opts = {})
+    Clients::Script.new(username, password, client_id, secret, opts)
   end
 end
