84475c8ad6192b4e27f5d55ec82ef83a59b795f6
SirNeon <sirneon618@gmail.com>
2016-04-05 22:35:37 -0400

    added clients module

--- lib/NeonRAW/clients/base.rb
+++ lib/NeonRAW/clients/base.rb
@@ -5,83 +5,85 @@ require_relative 'base/captcha'
 require_relative '../errors'
 
 module NeonRAW
-  # The underlying base for the client
-  class Base
-    include Base::Listings
-    include Base::Captchas
-    include Errors
+  module Clients
+    # The underlying base for the client
+    class Base
+      include Base::Listings
+      include Base::Captchas
+      include Errors
 
-    # Creates headers for oAuth2 requests.
-    # @!method api_headers
-    # @return [Hash] Returns oAuth2 headers.
-    def api_headers
-      {
-        'User-Agent' => @user_agent,
-        'Authorization' => "bearer #{@access.access_token}"
-      }
-    end
+      # Creates headers for oAuth2 requests.
+      # @!method api_headers
+      # @return [Hash] Returns oAuth2 headers.
+      def api_headers
+        {
+          'User-Agent' => @user_agent,
+          'Authorization' => "bearer #{@access.access_token}"
+        }
+      end
 
-    # Connects to Reddit for oAuth2 requests.
-    # @!method api_connection(path, meth, params)
-    # @param path [String] The API path.
-    # @param meth [Symbol] The request method.
-    # @param params [Hash] The parameters.
-    # @return [Typhoeus::Response] Returns the response.
-    def api_connection(path, meth, params)
-      response = Typhoeus::Request.new(
-        'https://oauth.reddit.com' + path,
-        method: meth,
-        headers: api_headers,
-        params: params
-      ).run
-      error = assign_errors(response)
-      fail error unless error.nil?
-      handle_ratelimit(response.headers)
-      response
-    end
+      # Connects to Reddit for oAuth2 requests.
+      # @!method api_connection(path, meth, params)
+      # @param path [String] The API path.
+      # @param meth [Symbol] The request method.
+      # @param params [Hash] The parameters.
+      # @return [Typhoeus::Response] Returns the response.
+      def api_connection(path, meth, params)
+        response = Typhoeus::Request.new(
+          'https://oauth.reddit.com' + path,
+          method: meth,
+          headers: api_headers,
+          params: params
+        ).run
+        error = assign_errors(response)
+        fail error unless error.nil?
+        handle_ratelimit(response.headers)
+        response
+      end
 
-    # Makes the connection used to authorize the client.
-    # @!method auth_connection(path, meth, params)
-    # @param path [String] The API path.
-    # @param meth [Symbol] The request method.
-    # @param params [Hash] The parameters.
-    # @return [Typhoeus::Response] Returns the response.
-    def auth_connection(path, meth, params)
-      response = Typhoeus::Request.new(
-        'https://www.reddit.com' + path,
-        method: meth,
-        userpwd: "#{@client_id}:#{@secret}",
-        headers: { 'User-Agent' => @user_agent },
-        params: params
-      ).run
-      error = assign_errors(response)
-      fail error unless error.nil?
-      response
-    end
+      # Makes the connection used to authorize the client.
+      # @!method auth_connection(path, meth, params)
+      # @param path [String] The API path.
+      # @param meth [Symbol] The request method.
+      # @param params [Hash] The parameters.
+      # @return [Typhoeus::Response] Returns the response.
+      def auth_connection(path, meth, params)
+        response = Typhoeus::Request.new(
+          'https://www.reddit.com' + path,
+          method: meth,
+          userpwd: "#{@client_id}:#{@secret}",
+          headers: { 'User-Agent' => @user_agent },
+          params: params
+        ).run
+        error = assign_errors(response)
+        fail error unless error.nil?
+        response
+      end
 
-    # Requests data from Reddit.
-    # @!method request_data(path, meth, params = {})
-    # @param path [String] The API path to connect to.
-    # @param meth [Symbol] The request method to use.
-    # @param params [Hash] Parameters for the request.
-    # @return [Hash] Returns the parsed JSON as a hash containing
-    #   the data.
-    def request_data(path, meth, params = {})
-      refresh_access! if @access.expired?
-      response = api_connection(path, meth, params)
-      data = JSON.parse(response.body, symbolize_names: true)
-      handle_data_errors(data)
-      data
-    end
+      # Requests data from Reddit.
+      # @!method request_data(path, meth, params = {})
+      # @param path [String] The API path to connect to.
+      # @param meth [Symbol] The request method to use.
+      # @param params [Hash] Parameters for the request.
+      # @return [Hash] Returns the parsed JSON as a hash containing
+      #   the data.
+      def request_data(path, meth, params = {})
+        refresh_access! if @access.expired?
+        response = api_connection(path, meth, params)
+        data = JSON.parse(response.body, symbolize_names: true)
+        handle_data_errors(data)
+        data
+      end
 
-    # Requests non-JSON data from Reddit.
-    # @!method request_nonjson(path, meth, params = {})
-    # @param path [String] The API path to connect to.
-    # @param meth [Symbol] The request method to use.
-    # @param params [Hash] Parameters for the request.
-    def request_nonjson(path, meth, params = {})
-      refresh_access! if @access.expired?
-      api_connection(path, meth, params).body
+      # Requests non-JSON data from Reddit.
+      # @!method request_nonjson(path, meth, params = {})
+      # @param path [String] The API path to connect to.
+      # @param meth [Symbol] The request method to use.
+      # @param params [Hash] Parameters for the request.
+      def request_nonjson(path, meth, params = {})
+        refresh_access! if @access.expired?
+        api_connection(path, meth, params).body
+      end
     end
   end
 end
--- lib/NeonRAW/clients/script.rb
+++ lib/NeonRAW/clients/script.rb
@@ -1,33 +1,34 @@
-require 'securerandom'
 require_relative 'base'
 
 module NeonRAW
-  # le script
-  class Script < Base
-    def initialize(username, password, client_id, secret, opts = {})
-      @username = username
-      @password = password
-      @client_id = client_id
-      @secret = secret
-      @redirect_uri = opts[:redirect_uri] || 'http://127.0.0.1:'
-      @user_agent = opts[:user_agent] ||
-                    "Powered by NeonRAW v#{NeonRAW::VERSION}"
-      authorize!
-    end
+  module Clients
+    # le script
+    class Script < Base
+      def initialize(username, password, client_id, secret, opts = {})
+        @username = username
+        @password = password
+        @client_id = client_id
+        @secret = secret
+        @redirect_uri = opts[:redirect_uri] || 'http://127.0.0.1:'
+        @user_agent = opts[:user_agent] ||
+                      "Powered by NeonRAW v#{NeonRAW::VERSION}"
+        authorize!
+      end
 
-    # Authorizes the client for oAuth2 requests.
-    # @!method authorize!
-    # @!method refresh_access!
-    def authorize!
-      response = auth_connection(
-        '/api/v1/access_token', :post,
-        grant_type: 'password',
-        username: @username,
-        password: @password
-      )
-      data = JSON.parse(response.body, symbolize_names: true)
-      @access = Objects::Access.new(data)
+      # Authorizes the client for oAuth2 requests.
+      # @!method authorize!
+      # @!method refresh_access!
+      def authorize!
+        response = auth_connection(
+          '/api/v1/access_token', :post,
+          grant_type: 'password',
+          username: @username,
+          password: @password
+        )
+        data = JSON.parse(response.body, symbolize_names: true)
+        @access = Objects::Access.new(data)
+      end
+      alias refresh_access! authorize!
     end
-    alias refresh_access! authorize!
   end
 end
