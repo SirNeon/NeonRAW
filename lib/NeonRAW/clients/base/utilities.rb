6671df33d418c0b4d78f9a036095492b21bed179
SirNeon <sirneon618@gmail.com>
2016-04-11 05:35:11 -0400

    added methods to fetch subreddits

--- lib/NeonRAW/clients/base/utilities.rb
+++ lib/NeonRAW/clients/base/utilities.rb
@@ -13,6 +13,27 @@ module NeonRAW
           data = request_data('/api/subreddits_by_topic', :get, params)
           data.map { |subreddit| subreddit[:name] }
         end
+
+        # Fetches subreddits.
+        # @!method get_popular(params = { limit: 25 })
+        # @!method get_new(params = { limit: 25 })
+        # @!method get_gold(params = { limit: 25 })
+        # @!method get_default(params = { limit: 25 })
+        # @param params [Hash] The parameters.
+        # @option params :after [String] Fullname of the next data block.
+        # @option params :before [String] Fullname of the previous data block.
+        # @option params :count [Integer] The number of items already in the
+        #   listing.
+        # @option params :limit [1..1000] The number of listing items to fetch.
+        # @option params :show [String] Literally the string 'all'.
+        # @return [NeonRAW::Objects::Listing] Returns a listing of all the
+        #   subreddits.
+        %w(popular new gold defaults).each do |type|
+          define_method :"get_#{type}" do |params = { limit: 25 }|
+            type.chop! if type == 'defaults'
+            build_listing("/subreddits/#{type}", params)
+          end
+        end
       end
     end
   end
