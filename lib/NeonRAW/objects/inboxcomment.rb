81a9d78dd7713c0fca53b0d4c0abcec8a9ba4a8f
SirNeon <sirneon618@gmail.com>
2016-04-14 16:06:39 -0400

    added docs and included more mixins

--- lib/NeonRAW/objects/inboxcomment.rb
+++ lib/NeonRAW/objects/inboxcomment.rb
@@ -3,11 +3,36 @@ require_relative 'thing'
 module NeonRAW
   module Objects
     # le inbox comment object
+    # @!attribute [r] body
+    #   @return [String, nil] Returns the text body of the comment or nil if
+    #     there is none.
+    # @!attribute [r] link_title
+    #   @return [String] Returns the title of the submission where the comment
+    #     was posted.
+    # @!attribute [r] dest
+    #   @return [String] Returns whom the InboxComment was sent to.
+    # @!attribute [r] author
+    #   @return [String] Returns the author of the comment.
+    # @!attribute [r] body_html
+    #   @return [String, nil] Returns the text body of the comment with HTML or
+    #     nil if there is none.
+    # @!attribute [r] subreddit
+    #   @return [String] Returns the subreddit where the comment was posted.
+    # @!attribute [r] parent_id
+    #   @return [String] Returns the fullname of the comment's parent object.
+    # @!attribute [r] context
+    #   @return [String] Returns a link to the comment with context provided.
+    # @!attribute [r] new?
+    #   @return [Boolean] Returns whether the comment is new or not.
+    # @!attribute [r] subject
+    #   @return [String] Returns the subject of the comment (post/comment
+    #     reply).
     class InboxComment < Thing
       include Thing::Createable
       include Thing::Inboxable
       include Thing::Moderateable
       include Thing::Repliable
+      include Thing::Votable
 
       # @!method initialize(client, data)
       # @param client [NeonRAW::Clients::Web/Installed/Script] The client.
@@ -19,6 +44,9 @@ module NeonRAW
           instance_variable_set(:"@#{key}", value)
           next if key == :created || key == :created_utc
           self.class.send(:attr_reader, key)
+        end
+        class << self
+          alias_method :new?, :new
         end
       end
     end
