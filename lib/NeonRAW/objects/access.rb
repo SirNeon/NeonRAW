f4677f314dd4faf11795d0ba89090b163d6697f4
SirNeon <sirneon618@gmail.com>
2016-04-19 21:23:51 -0400

    added refresh method and touched up docs

--- lib/NeonRAW/objects/access.rb
+++ lib/NeonRAW/objects/access.rb
@@ -5,16 +5,19 @@ module NeonRAW
     #   @return [String] Returns the access token used for oAuth2.
     # @!attribute [r] token_type
     #   @return [String] Returns the type of the token (bearer)
+    # @!attribute [r] refresh_token
+    #   @return [String] Returns the refresh token.
     # @!attribute [r] scope
     #   @return [String] Returns the scope where the token is valid.
     # @!attribute [r] expires_in
     #   @return [Time] Returns how long until the token expires.
     # @!attribute [r] expires_at
     #   @return [Time] Returns when the token expires.
+    # @note refresh_token is only present in the Web/Installed clients.
     class Access
       def initialize(data)
         data.each do |key, value|
-          instance_variable_set("@#{key}", value)
+          instance_variable_set(:"@#{key}", value)
           self.class.send(:attr_reader, key)
         end
         # I have it expire 10 seconds early to give a small buffer
@@ -26,6 +29,13 @@ module NeonRAW
       # @return [Boolean] Returns whether or not the token is expired.
       def expired?
         Time.now > @expires_at
+      end
+
+      # Refresh the access token.
+      # @!method refresh!(data)
+      # @param data [Hash] The new data.
+      def refresh!(data)
+        data.each { |key, value| instance_variable_set(:"@#{key}", value) }
       end
     end
   end
