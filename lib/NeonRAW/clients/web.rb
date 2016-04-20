36af04c301622202ec5d53fe5077e9f98c47ac48
SirNeon <sirneon618@gmail.com>
2016-04-19 21:23:05 -0400

    added web client

---
+++ lib/NeonRAW/clients/web.rb
@@ -0,0 +1,53 @@
+require 'uri'
+require_relative 'base'
+
+module NeonRAW
+  module Clients
+    # The Web app client.
+    class Web < Base
+      def initialize(client_id, secret, redirect_uri, opts = {})
+        @client_id = client_id
+        @secret = secret
+        @redirect_uri = redirect_uri
+        @user_agent = opts[:user_agent] ||
+                      "Powered by NeonRAW v#{NeonRAW::VERSION}"
+      end
+
+      # Generates the authorization URL.
+      # @!method auth_url(state, scope = ['*'], duration = 'permanent')
+      # @param state [String] A random string to check later.
+      # @param scope [Array<String>] The scopes your app uses.
+      # @param duration [String] The duration of the access token [temporary,
+      #   permanent].
+      # @return [String] Returns the URL.
+      def auth_url(state, scope = ['*'], duration = 'permanent')
+        query = {
+          response_type: 'code',
+          client_id: @client_id,
+          redirect_uri: @redirect_uri,
+          state: state,
+          scope: scope.join(',').chop,
+          duration: duration
+        }
+
+        url = URI.join('https://www.reddit.com', '/api/v1/authorize')
+        url.query = URI.encode_www_form(query)
+        url.to_s
+      end
+
+      # Authorizes the client.
+      # @!method authorize!(code)
+      # @param code [String] The authorization code.
+      def authorize!(code)
+        response = auth_connection(
+          '/api/v1/access_token', :post,
+          grant_type: 'authorization_code',
+          code: code,
+          redirect_uri: @redirect_uri
+        )
+        data = JSON.parse(response.body, symbolize_names: true)
+        @access = Objects::Access.new(data)
+      end
+    end
+  end
+end
