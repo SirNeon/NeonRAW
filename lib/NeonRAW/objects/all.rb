6e0d87ef0864893f52cdf805d508b04c4f151b91
SirNeon <sirneon618@gmail.com>
2016-04-20 12:36:14 -0400

    added object for /r/all

---
+++ lib/NeonRAW/objects/all.rb
@@ -0,0 +1,32 @@
+module NeonRAW
+  module Objects
+    # The object for /r/all.
+    class All
+      # @!group Listings
+      # Fetches a listing from /r/all.
+      # @!method hot(params = { limit: 25 })
+      # @!method rising(params = { limit: 25 })
+      # @!method top(params = { limit: 25 })
+      # @!method old(params = { limit: 25 })
+      # @!method new(params = { limit: 25 })
+      # @!method controversial(params = { limit: 25 })
+      # @!method comments(param = { limit: 25 })
+      # @param params [Hash] The parameters for the request.
+      # @option params :t [String] Time for relevant sorting [hour, day, week,
+      #   month, year, all]
+      # @option params :after [String] The name of the next data block.
+      # @option params :before [String] The name of the previous data block.
+      # @option params :count [Integer] The number of items already in the
+      #   listing.
+      # @option params :limit [1..1000] The number of items to fetch.
+      # @option params :show [String] Literally the string 'all'.
+      # @return [NeonRAW::Objects::Listing] Returns the listing object.
+      %w(hot rising top old new controversial comments).each do |type|
+        define_method :"#{type}" do |params = { limit: 25 }|
+          path = "/r/all/#{type}/.json"
+          @client.send(:build_listing, path, params)
+        end
+      end
+    end
+  end
+end
