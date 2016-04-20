977b3176a722106b11fc5e2ca7f70907e0515089
SirNeon <sirneon618@gmail.com>
2016-04-19 23:45:48 -0400

    made refresh token attribute more permanent

--- lib/NeonRAW/objects/access.rb
+++ lib/NeonRAW/objects/access.rb
@@ -6,19 +6,20 @@ module NeonRAW
     # @!attribute [r] token_type
     #   @return [String] Returns the type of the token (bearer)
     # @!attribute [r] refresh_token
-    #   @return [String] Returns the refresh token.
+    #   @return [String, nil] Returns the refresh token or nil if there is none.
     # @!attribute [r] scope
     #   @return [String] Returns the scope where the token is valid.
     # @!attribute [r] expires_in
     #   @return [Time] Returns how long until the token expires.
     # @!attribute [r] expires_at
     #   @return [Time] Returns when the token expires.
-    # @note refresh_token is only present in the Web/Installed client if the
-    #   duration is permanent.
     class Access
+      attr_reader :refresh_token
       def initialize(data)
+        @refresh_token = nil
         data.each do |key, value|
           instance_variable_set(:"@#{key}", value)
+          next if key == :refresh_token
           self.class.send(:attr_reader, key)
         end
         # I have it expire 10 seconds early to give a small buffer
