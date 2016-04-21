401837998ee8c5e7410c1e79d9e1051912720e72
SirNeon <sirneon618@gmail.com>
2016-04-20 23:46:25 -0400

    readded comment

--- lib/NeonRAW/objects/subreddit/utilities.rb
+++ lib/NeonRAW/objects/subreddit/utilities.rb
@@ -37,6 +37,7 @@ module NeonRAW
           params[:sr] = display_name
           params[:title] = title
           response = @client.request_data('/api/submit', :post, params)
+          # Seriously though, fucking convoluted data structures.
           submission_id = response[:jquery].last[3].first.split('/')[6]
           info(id: 't3_' + submission_id)
         end
