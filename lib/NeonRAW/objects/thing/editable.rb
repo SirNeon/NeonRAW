93352a4cddbf04139a90c9b3ab9cb7b263d49630
SirNeon618 <sirneon618@gmail.com>
2017-07-14 04:14:42 -0400

    improved consistency

--- lib/NeonRAW/objects/thing/editable.rb
+++ lib/NeonRAW/objects/thing/editable.rb
@@ -16,12 +16,16 @@ module NeonRAW
 
         # Gets the timestamp of the thing's lastest edit.
         # @!method last_edit
-        # @return [Float, nil] Returns the UNIX timestamp of the edit or nil if
+        # @return [Time, nil] Returns the Time of the edit or nil if
         #   the comment hasn't been edited.
         # @note If you crawl some old comments on /r/reddit.com this may return
         #   true instead of the timestamp.
         def last_edit
-          nil || @edited if @edited != false
+          if @edited == true
+            @edited
+          elsif @edited.is_a?(Float)
+            Time.at(@edited)
+          end
         end
 
         # Edit a thing.
